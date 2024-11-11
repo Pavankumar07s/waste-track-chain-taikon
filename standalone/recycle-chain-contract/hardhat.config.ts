3f538b896bc5f4c12a021618bcb5712af9ad61dd
Pavankumar07s <pawankumar14662693@gmail.com>
2024-11-11 13:53:38 +0530

    taiko build

--- standalone/recycle-chain-contract/hardhat.config.ts
+++ standalone/recycle-chain-contract/hardhat.config.ts
@@ -4,11 +4,15 @@ require('dotenv').config()
 
 const config: HardhatUserConfig = {
   solidity: '0.8.24',
-  defaultNetwork: 'amoy',
+  defaultNetwork: 'taiko',
   networks: {
     amoy: {
       url: `https://polygon-amoy.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
+    },
+    taiko: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.YOUR_PRIVATE_KEY],
     },
   },
 }
